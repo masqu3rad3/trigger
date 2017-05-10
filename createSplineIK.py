233a531698660175daac0504c34d1aa660f8c21f
Arda Kutlu <ardakutlu@gmail.com>
2017-05-10 18:18:22 +0200

    createSpline WIP, twsit needs to be added, scaling and volume preserving needs to be done

--- createSplineIK.py
+++ createSplineIK.py
@@ -2,27 +2,32 @@
 
 import pymel.core as pm
 
-refJoints=[pm.PyNode("joint1"), pm.PyNode("joint2"), pm.PyNode("joint3"), pm.PyNode("joint4")]
+import extraProcedures as extra
+reload(extra)
 
-cuts=5
 
+#refJoints=[pm.PyNode("joint1"), pm.PyNode("joint2"), pm.PyNode("joint3"), pm.PyNode("joint4"), pm.PyNode("joint5"), pm.PyNode("joint6"), pm.PyNode("joint7")]
+refJoints=pm.selected()
+
+dropoff=2
+cuts=32
+extra.getDistance(refJoints[0], refJoints[0])
 totalLength=0
 rootPoint=refJoints[0].getTranslation(space="world")
 splitCount=5
-contDistances=[0]
-for i in range (0, len(refJoints)):
-    currentJointLength=pm.getAttr(refJoints[i].tx)
-    totalLength+=currentJointLength
-    contDistances.append(currentJointLength)
-contDistances.remove(len(contDistances)-1)
-del contDistances[len(contDistances)-1]
-contDistances.delete(len(contDistances)-1)
-## Draw a new joint for spline IK
+contDistances=[]
+contCurves=[]
+ctrlDistance=0
 
-# starting point will be the same with the ref
-# pm.select(d=True)
-# splineRoot=pm.joint(p=rootPoint, name=("jDef_splineRoot"))
-# splineEnd=pm.joint(p=(rootPoint.x, (rootPoint.y+totalLength), rootPoint.z), name=("jDef_splineEnd"))
+for i in range (0, len(refJoints)):
+    if i==0:
+        tmin=0
+    else:
+        tmin=i-1
+    currentJointLength=extra.getDistance(refJoints[i], refJoints[tmin])
+    ctrlDistance=currentJointLength+ctrlDistance
+    totalLength+=currentJointLength
+    contDistances.append(ctrlDistance)
 
 rootVc=rootPoint
 endVc=(rootPoint.x, (rootPoint.y+totalLength), rootPoint.z)
@@ -32,22 +37,54 @@ segmentVc=(splitVc/(cuts))
 segmentLoc=rootVc+segmentVc
 curvePoints=[]
 splineJoints=[]
+pm.select(d=True)
+
+## Create deformation Joints
 for i in range (0, cuts+1):
     place=rootVc+(segmentVc*(i))
-    # pm.spaceLocator(p=place)
     j=pm.joint(p=place, name="jDef_spline"+str(i))
     splineJoints.append(j)
     curvePoints.append(place)
     
 splineCurve=pm.curve(name="splineCurve", p=curvePoints)
 
-splineIK=pm.ikHandle(sol="ikSplineSolver", c=splineCurve, sj=splineJoints[0], ee=splineJoints[len(splineJoints)-1], w=1.0)
+splineIK=pm.ikHandle(sol="ikSplineSolver", createCurve=False, c=splineCurve, sj=splineJoints[0], ee=splineJoints[len(splineJoints)-1], w=1.0)
 
 ## Create controller Joints
 contJoints=[]
 pm.select(d=True)
-for i in range (0, cuts+1):
-    place=rootVc*(contDistances[i])
-    j=pm.joint(p=place, name="jCont_spline"+str(i))
+for i in range (0, len(contDistances)):
+    ctrlVc=splitVc.normal()*contDistances[i]
+    place=rootVc+(ctrlVc)
+    j=pm.joint(p=place, name="jCont_spline"+str(i), radius=5)
     contJoints.append(j)
     pm.select(d=True)
+    
+pm.select(contJoints)
+pm.select(splineCurve, add=True)
+pm.skinCluster(dr=dropoff, tsb=True)
+
+for i in range (0, len(contJoints)):
+    extra.alignTo(contJoints[i], refJoints[i],0)
+    ## Create control Curves
+    cont_Curve=pm.circle(name="cont_spline_"+str(i), nr=(0,1,0))
+    scaleRatio=(totalLength/len(contJoints))
+    pm.setAttr(cont_Curve[0].scale, (scaleRatio, scaleRatio, scaleRatio))
+    cont_Curve_ORE=extra.createUpGrp(cont_Curve[0],"ORE")
+    extra.alignTo(cont_Curve_ORE, contJoints[i],2)
+    pm.parentConstraint(cont_Curve[0], contJoints[i])
+    contCurves.append(cont_Curve)
+
+    
+## Create Stretch and Squash Nodes
+curveInfo=pm.arclen(splineCurve, ch=True)
+initialLength=pm.getAttr(curveInfo.arcLength)
+for i in range (0, len(splineJoints)):
+    
+
+    lengthMult=pm.createNode("multiplyDivide")
+    pm.setAttr(lengthMult.operation, 2)
+    curveInfo.arcLength >> lengthMult.input1X
+    pm.setAttr(lengthMult.input2X, initialLength)
+    lengthMult.outputX >> splineJoints[i].sy
+    
