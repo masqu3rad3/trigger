56ff7c0857a192324dc8d601db474902dbea04a5
Arda Kutlu <ardakutlu@gmail.com>
2020-04-14 10:48:44 +0100

    boiler_plate module added

---
+++ trigger/modules/boiler_plate.py
@@ -0,0 +1,104 @@
+from maya import cmds
+from trigger.library import functions as extra
+from trigger.library import controllers as ic
+
+class Limb(object):
+    def __init__(self, inits, suffix="", side="L"):
+
+        # fool proofing
+
+        # reinitialize the initial Joints
+        self.inits=inits
+
+        # get distances
+
+        # get positions
+
+        # initialize sides
+        self.sideMult = -1 if side == "R" else 1
+        self.side = side
+
+        # initialize coordinates
+        self.up_axis, self.mirror_axis, self.look_axis = extra.getRigAxes(self.inits[0])
+
+        # initialize suffix
+        self.suffix = (extra.uniqueName("limbGrp_%s" % suffix)).replace("limbGrp_", "")
+
+        # scratch variables
+        self.sockets = []
+        self.limbGrp = None
+        self.scaleGrp = None
+        self.nonScaleGrp = None
+        self.limbPlug = None
+        self.scaleConstraints = []
+        self.anchors = []
+        self.anchorLocations = []
+        self.deformerJoints = []
+        self.colorCodes = [6, 18]
+
+    def createGrp(self):
+        self.limbGrp = cmds.group(name="limbGrp_%s" % self.suffix, em=True)
+        self.scaleGrp = cmds.group(name="scaleGrp_%s" % self.suffix, em=True)
+        extra.alignTo(self.scaleGrp, self.inits[0], 0)
+        self.nonScaleGrp = cmds.group(name="NonScaleGrp_%s" % self.suffix, em=True)
+
+        cmds.addAttr(self.scaleGrp, at="bool", ln="Control_Visibility", sn="contVis", defaultValue=True)
+        cmds.addAttr(self.scaleGrp, at="bool", ln="Joints_Visibility", sn="jointVis", defaultValue=True)
+        cmds.addAttr(self.scaleGrp, at="bool", ln="Rig_Visibility", sn="rigVis", defaultValue=False)
+        # make the created attributes visible in the channelbox
+        cmds.setAttr("%s.contVis" % self.scaleGrp, cb=True)
+        cmds.setAttr("%s.jointVis" % self.scaleGrp, cb=True)
+        cmds.setAttr("%s.rigVis" % self.scaleGrp, cb=True)
+
+        cmds.parent(self.scaleGrp, self.limbGrp)
+        cmds.parent(self.nonScaleGrp, self.limbGrp)
+
+    def createJoints(self):
+        # draw Joints
+
+        # orientations
+        pass
+
+    def createControllers(self):
+        pass
+
+    def createRoots(self):
+        pass
+
+    def createIKsetup(self):
+        pass
+
+    def createFKsetup(self):
+        pass
+
+    def ikfkSwitching(self):
+        pass
+
+    def createRibbons(self):
+        pass
+
+    def createTwistSplines(self):
+        pass
+
+    def createAngleExtractors(self):
+        pass
+
+    def roundUp(self):
+        cmds.parentConstraint(self.limbPlug, self.scaleGrp, mo=False)
+        cmds.setAttr("%s.rigVis" % self.scaleGrp, 0)
+
+        self.scaleConstraints.append(self.scaleGrp)
+        # lock and hide
+
+    def createLimb(self):
+        self.createGrp()
+        self.createJoints()
+        self.createControllers()
+        self.createRoots()
+        self.createIKsetup()
+        self.createFKsetup()
+        self.ikfkSwitching()
+        self.createRibbons()
+        self.createTwistSplines()
+        self.createAngleExtractors()
+        self.roundUp()
