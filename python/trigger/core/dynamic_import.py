2d935d4199a91b44a29df7a1fb163f3f3b54afb4
Arda Kutlu <arda.kutlu@rebellion.co.uk>
2021-02-24 09:32:27 +0000

    ends

--- python/trigger/core/dynamic_import.py
+++ python/trigger/core/dynamic_import.py
@@ -1,17 +1,5 @@
 """Module for dynamic Imports"""
 import os, sys
-#
-# python_version = sys.version_info.major
-# if python_version < 2:
-#     import importlib as imp
-# else:
-#     import imp
-#
-# def dynamic_import(file_path):
-#     module_name = os.path.splitext(os.path.basename(file_path))[0]
-#     module = imp.load_source(module_name, file_path)
-#     return module
-
 
 def dynamic_import(file_path):
     module_name = os.path.splitext(os.path.basename(file_path))[0]
