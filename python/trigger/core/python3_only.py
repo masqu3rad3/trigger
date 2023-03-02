89385644a8abf727edb363677da3fe209f361c82
Arda Kutlu <ardakutlu@gmail.com>
2023-03-02 21:29:12 +0000

    added export guides button and added status bar update informations. Scale values on guide export hard coded for 1, 1, 1

--- python/trigger/core/python3_only.py
+++ python/trigger/core/python3_only.py
@@ -2,7 +2,7 @@ import collections
 
 def flatten(l):
     for el in l:
-        if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+        if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
             yield from flatten(el)
         else:
             yield el
